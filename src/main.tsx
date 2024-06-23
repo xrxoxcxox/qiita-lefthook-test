f0e243dc13a49ccfb7898b7924741c1ad9491bc1
Keisuke Watanuki <watanuki@ju-zen.com>
2024-06-24 04:42:15 +0900

    Test commit (for pre-push)

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-const rootElement = document.getElementById('root')
-
-ReactDOM.createRoot(rootElement).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
